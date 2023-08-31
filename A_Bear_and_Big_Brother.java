dfba684992e539f24573e583aee5daa08afd3223
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-01 01:04:15 +0530

    1-9

--- A_Bear_and_Big_Brother.java
+++ A_Bear_and_Big_Brother.java
@@ -1,4 +1,8 @@
+//https://codeforces.com/problemset/problem/791/A
 import java.util.*;
+
+import java.util.Scanner;
+
 public class A_Bear_and_Big_Brother{
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);

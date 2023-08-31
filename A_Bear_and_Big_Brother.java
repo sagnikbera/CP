9c20e1a3b892f4c556fafd1ebd2cf53eeb37cf0b
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-01 01:03:21 +0530

    1-9

---
+++ A_Bear_and_Big_Brother.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class A_Bear_and_Big_Brother{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+    int a= sc.nextInt();
+    int b=sc.nextInt();
+
+   int count = 0;
+   while(a<=b){
+    count++;
+    a=a*3;
+    b=b*2;
+   } 
+   System.out.println(count);
+}
+}

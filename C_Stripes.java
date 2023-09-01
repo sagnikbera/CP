e69d682239df099f52313077f20fe719936a1068
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-01 22:24:20 +0530

    1-9

--- C_Stripes.java
+++ C_Stripes.java
@@ -1,4 +1,9 @@
+//https://codeforces.com/contest/1742/problem/C
+
 import java.util.*;
+
+import java.util.Scanner;
+
 public class C_Stripes{
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);

f7a40b0019b7d4599527d8596d91bf4ab15cf6f5
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-02 00:01:09 +0530

    x

---
+++ A_Soldier_and_Bananas.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class A_Soldier_and_Bananas{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+    int k = sc.nextInt();
+    int n = sc.nextInt();
+    int w = sc.nextInt();
+
+    int x = k*w*(w+1)/2;
+    System.out.println(x-n);
+}
+}

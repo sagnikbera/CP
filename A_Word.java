510c683ea8bb241c501112d88b960c60ea5128a6
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-04 00:06:59 +0530

    4-9

---
+++ A_Word.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class A_Word{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+String s = sc.next();
+int k=0 , l=0;
+for(int i=0;i<s.length();i++){
+    int x= s.charAt(i);
+    if(x>=97){
+        k++;
+    }else{
+        l++;
+    }
+}
+
+if(k>=l){
+    System.out.println(s.toLowerCase());
+}else{
+    System.out.println(s.toUpperCase());
+}
+}
+}
+
+
+

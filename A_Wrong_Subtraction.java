6e6c6bbacf0777b7af07a222756711879f1b352d
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-04 15:15:43 +0530

    4-9

---
+++ A_Wrong_Subtraction.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class A_Wrong_Subtraction{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+int k = sc.nextInt();
+while(k-->0){
+    if(n%10!=0){
+        n=n-1;
+    }else if(n%10==0){
+        n=n/10;
+    }
+}
+System.out.println(n);
+}
+}

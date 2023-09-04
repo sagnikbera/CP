6812d02846545abdbb013dc7f5d803369073ca90
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-05 02:12:53 +0530

    5-9

--- A_Nearly_Lucky_Number.java
+++ A_Nearly_Lucky_Number.java
@@ -7,19 +7,24 @@ public class A_Nearly_Lucky_Number{
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 long n = sc.nextLong();
-int count=0;
-while(n>0){
-    
-    long f=n%10;
-    if(f!=4 && f!=7){
-        count++;
-    }
-    n=n/10;
+int c=0;
+while(n>0)
+{
+
+if(n%10==4||n%10==7)
+{
+c++;
 }
-if(count>0){
-    System.out.println("NO");
-}else{
-    System.out.println("YES");
+
+n/=10;
+
+}
+if(c==4||c==7)
+{
+System.out.println("YES");
+}
+else{
+System.out.println("NO");
 }
 }
 }

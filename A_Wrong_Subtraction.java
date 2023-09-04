3d1690da78762e0d16b8ace545d78d46cf454b39
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-04 15:17:06 +0530

    4-9

--- A_Wrong_Subtraction.java
+++ A_Wrong_Subtraction.java
@@ -1,4 +1,8 @@
+//https://codeforces.com/problemset/problem/977/A
 import java.util.*;
+
+import java.util.Scanner;
+
 public class A_Wrong_Subtraction{
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);

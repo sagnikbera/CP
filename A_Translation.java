f689857ebc560cbd0e404fbe9a8df47598b60ef3
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-07 01:35:47 +0530

    7-9

---
+++ A_Translation.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class A_Translation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String s = scanner.nextLine();
+        String t = scanner.nextLine();
+
+
+        if (s.equals(new StringBuilder(t).reverse().toString())) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        scanner.close();
+    }
+}

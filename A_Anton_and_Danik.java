5ef5f3c3e70221475b925b3ff1b0bdd5dcd761ba
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-06 09:51:08 +0530

    6-9

---
+++ A_Anton_and_Danik.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class AD
+{
+	public static void main(String args[]){
+	int ag=0,dg=0;
+	Scanner s=new Scanner(System.in);
+	int a=s.nextInt();
+	String str=s.next();
+	for(int i=0;i<a;i++){
+		if(str.charAt(i)=='A')
+			ag++;
+		else 
+			dg++;
+	}
+	if(ag>dg)
+		System.out.println("Anton");
+	else if(ag<dg)
+		System.out.println("Danik");
+	else
+		System.out.println("Friendship");
+}
+}

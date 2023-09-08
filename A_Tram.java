dfcb660e166efa63ccd80d6d58788d69e5622b3b
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-08 10:22:05 +0530

    8-9

---
+++ A_Tram.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class A_Tram{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+int n=sc.nextInt();
+int count = 0;
+int max=0;
+while(n-->0){
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    count = count + (b - a);
+    if(count>max){
+        max = count;
+    }
+}
+System.out.println(max);
+}
+}

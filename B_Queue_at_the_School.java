b65ba0d074d08c886080c4f0785ce9b3d3a5b743
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-10 18:44:34 +0530

    10-9

---
+++ B_Queue_at_the_School.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class B_Queue_at_the_School{
+	public static void main(String[] A) {
+		Scanner s = new Scanner(System.in);
+		s.next();
+		int t = s.nextInt();
+		String a = s.next();
+		for (; t-- > 0;)
+			a = a.replaceAll("BG", "GB");
+		System.out.print(a);
+	}
+}

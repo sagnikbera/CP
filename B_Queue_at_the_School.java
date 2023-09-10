739011fc14bc7ecb4cf40f7e8fd236105d0e7923
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-10 18:45:07 +0530

    10-9

--- B_Queue_at_the_School.java
+++ B_Queue_at_the_School.java
@@ -1,4 +1,8 @@
+//https://codeforces.com/problemset/problem/266/B
 import java.util.Scanner;
+
+import java.util.Scanner;
+
 public class B_Queue_at_the_School{
 	public static void main(String[] A) {
 		Scanner s = new Scanner(System.in);

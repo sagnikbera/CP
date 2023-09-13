01cf9ecc73a11fb14f1bb720e219f43d9ef203b3
sagnikbera <sagnikberaofficial@gmail.com>
2023-09-14 00:02:34 +0530

    14-9

--- A_Hayato_and_School.java
+++ A_Hayato_and_School.java
@@ -1,7 +1,7 @@
 //https://codeforces.com/contest/1780/problem/A
 
 import java.util.*;
-public class NAME{
+public class A_Hayato_and_School{
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 }
